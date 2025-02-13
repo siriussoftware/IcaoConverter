b28a0c528b5790293220f8824525e6f59ee6d31b
sirius <sirius_software@earthlink.net>
2025-02-13 15:40:11 -0600

    Yeah, working ICAO conversion routines

---
+++ ICAO_convert/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ICAO_convert;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string code = ICAOConverter.icao_to_n("a45935");
+            Console.WriteLine(code);
+            Console.WriteLine(ICAOConverter.NToIcao(code));
+        }
+    }
